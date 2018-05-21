12e3bb22b01545694aee943b50c7c8941d0a044b
sol <pdpxpd@gmail.com>
2018-05-21 01:33:52 +0000

    init project

---
+++ app/controllers/button_controller.rb
@@ -0,0 +1,23 @@
+class ButtonController < ApplicationController
+    @@clicked = false
+    
+    def index
+        
+    end
+    
+    def audience
+        
+    end
+    
+    def speaker
+        
+    end
+    
+    def check
+        
+    end
+    
+    def click
+        
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'button#index'
+  
+  get '/audience' => 'button#audience'
+  get '/speaker' => 'button#speaker'
+  
+  post 'button#check'
+  post 'button#click'
+end
